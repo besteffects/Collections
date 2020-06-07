717911c18385ea6ff6e21cefd47ee88aa5f288d6
Vit <besteffects@gmail.com>
2020-06-07 17:58:21 -0400

    CollectionConcepts. Iterator

--- CollectionsCourse/src/Arrays/CollectionConcepts.java
+++ CollectionsCourse/src/Arrays/CollectionConcepts.java
@@ -2,6 +2,7 @@ package Arrays;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class CollectionConcepts {
     public static void main(String[] args) {
@@ -9,11 +10,26 @@ public class CollectionConcepts {
         Product floorPanel = new Product("Floor Panel", 25);
         Product window = new Product("Glass Window", 10);
 
-        Collection <Product> products = new ArrayList<Product>();
+        Collection<Product> products = new ArrayList<Product>();
         products.add(door);
         products.add(floorPanel);
         products.add(window);
 
-        System.out.println(products);
+        // System.out.println(products);
+
+        final Iterator<Product> productIterator = products.iterator();
+
+        //Printing with while:
+//        while (productIterator.hasNext()){
+//            Product product = productIterator.next();
+//            System.out.println(product);
+//        }
+
+        //Printing with foreach
+        for (Product product : products) {
+            System.out.println(product);
+        }
+
+
     }
 }
