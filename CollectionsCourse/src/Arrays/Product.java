4cd0f67b32cc6fcecae729b04f0fafbe563055e5
Vit <besteffects@gmail.com>
2020-06-08 23:33:48 -0400

    added comparator

--- CollectionsCourse/src/Arrays/Product.java
+++ CollectionsCourse/src/Arrays/Product.java
@@ -1,7 +1,18 @@
 package Arrays;
 
+import java.util.Comparator;
+
 public class Product
 {
+    public static final Comparator<Product> BY_WEIGHT
+            = new Comparator<Product>()
+    {
+        public int compare(final Product p1, final Product p2)
+        {
+            return Integer.compare(p1.getWeight(), p2.getWeight());
+        }
+    };
+
     private final String name;
     private final int weight;
 
