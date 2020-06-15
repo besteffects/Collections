fa4ba168dff67df710221ce3ebef6d2b2d95c8b9
Vit <besteffects@gmail.com>
2020-06-14 20:03:25 -0400

    shouldNotReplaceMissingItems test

--- CollectionsCourse/src/main/java/_3_Lists/Shipment.java
+++ CollectionsCourse/src/main/java/_3_Lists/Shipment.java
@@ -11,28 +11,49 @@ public class Shipment implements Iterable<Product> {
     private static int LIGHT_VAN_MAX_WEIGHT = 20;
     private final List<Product> products = new ArrayList<>();
 
+    private List<Product>lightVanProducts;
+    private List<Product>heavyVanProducts;
+
     public void add(Product product) {
-        //TODO
         products.add(product);
     }
 
     public void replace(Product oldProduct, Product newProduct) {
         final int index = products.indexOf(oldProduct);
-        if (index!=PRODUCT_NOT_PRESENT) {
+        if (index != PRODUCT_NOT_PRESENT) {
             products.set(index, newProduct);
         }
     }
 
     public void prepare() {
-        //TODO
+        //1 sort our list of products by weight
+
+        //sorting with Java 8
+        products.sort(Product.BY_WEIGHT);
+
+        //find the product index that needs the heavy van
+        int splitPOint = findSplitPoint();
+        //assign views of the product list for heavy and light vans
+    lightVanProducts=products.subList(0,splitPOint);
+    heavyVanProducts=products.subList(splitPOint, products.size());
+    }
+
+    private int findSplitPoint() {
+        for (int i = 0; i < products.size(); i++) {
+            final Product product = products.get(i);
+            if (product.getWeight()>LIGHT_VAN_MAX_WEIGHT){
+                return i;
+            }
+        }
+        return 0;
     }
 
     public List<Product> getHeavyVanProducts() {
-        return null;
+        return heavyVanProducts;
     }
 
     public List<Product> getLightProducts() {
-        return null;
+        return lightVanProducts;
     }
 
     public Iterator<Product> iterator() {
--- CollectionsCourse/src/test/java/demo/ShipmentTest.java
+++ CollectionsCourse/src/test/java/demo/ShipmentTest.java
@@ -13,7 +13,7 @@ public class ShipmentTest {
     private Shipment shipment = new Shipment();
 
     @Test
-    public void shouldAddItems() throws Exception{
+    public void shouldAddItems() throws Exception {
         shipment.add(door);
         shipment.add(window);
         shipment.replace(door, floorPanel);
@@ -22,18 +22,29 @@ public class ShipmentTest {
     }
 
     @Test
-    public void shouldReplaceItems ()throws Exception {
+    public void shouldReplaceItems() throws Exception {
         shipment.add(door);
         shipment.add(window);
-        shipment.replace(door,floorPanel);
+        shipment.replace(door, floorPanel);
 
-        assertThat(shipment,contains(floorPanel,window));
+        assertThat(shipment, contains(floorPanel, window));
     }
 
     @Test
-    public void shouldNotReplaceMissingItems() throws Exception{
+    public void shouldNotReplaceMissingItems() throws Exception {
         shipment.add(window);
-        shipment.replace(door,floorPanel);
-        assertThat(shipment,contains(window));
+        shipment.replace(door, floorPanel);
+        assertThat(shipment, contains(window));
+    }
+
+    @Test
+    public void shouldIdentifyVanRequirements() throws Exception {
+        shipment.add(door);
+        shipment.add(window);
+        shipment.add(floorPanel);
+        shipment.prepare();
+
+        assertThat(shipment.getLightProducts(), contains(window));
+        assertThat(shipment.getHeavyVanProducts(), contains(floorPanel, door));
     }
 }
