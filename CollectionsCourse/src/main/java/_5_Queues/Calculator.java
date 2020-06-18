8f4d536580b258bb8d7e6115b22ba54f0ad59d7d
Vit <besteffects@gmail.com>
2020-06-18 17:56:01 -0400

    tests for Calculator

---
+++ CollectionsCourse/src/main/java/_5_Queues/Calculator.java
@@ -0,0 +1,8 @@
+package _5_Queues;
+
+public class Calculator {
+
+    public int evaluate (final String input){
+        return 0;
+    }
+}
---
+++ CollectionsCourse/src/test/java/demo/CalculatorTest.java
@@ -0,0 +1,34 @@
+package demo;
+
+import _5_Queues.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void shouldEvaluateConstants() {
+        int result = calculator.evaluate("1");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldSupportAdding() {
+        int result = calculator.evaluate("1+2");
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void shouldSupportSubstruction() {
+        int result = calculator.evaluate("1-2");
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void shouldComplexStatements() {
+        int result = calculator.evaluate("1-3+2+4");
+        assertEquals(4, result);
+    }
+}
