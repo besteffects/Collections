37254f9cb7e362b50f2ff12974b54b0a88e3b9f9
Vit <besteffects@gmail.com>
2020-06-21 01:37:26 -0400

    ViewsOverMaps

---
+++ CollectionsCourse/src/main/java/_6_Maps/ProductFixturesMap.java
@@ -0,0 +1,7 @@
+package _6_Maps;
+
+public class ProductFixturesMap {
+    public static ProductMap door = new ProductMap(1, "Wooden Door", 35);
+    public static ProductMap floorPanel = new ProductMap(2, "Floor Panel", 25);
+    public static ProductMap window = new ProductMap(3, "Glass Window", 10);
+}
--- CollectionsCourse/src/main/java/_6_Maps/ViewsOverMaps.java
+++ CollectionsCourse/src/main/java/_6_Maps/ViewsOverMaps.java
@@ -1,4 +1,25 @@
 package _6_Maps;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class ViewsOverMaps {
+    public static void main(String[] args) {
+        final Map<Integer, ProductMap> idToProduct = new HashMap<>();
+        idToProduct.put(1, ProductFixturesMap.door);
+        idToProduct.put(2, ProductFixturesMap.floorPanel);
+        idToProduct.put(3, ProductFixturesMap.window);
+
+        System.out.println(idToProduct);
+        System.out.println();
+
+        final Set<Integer> ids =idToProduct.keySet();
+        System.out.println(ids);
+
+        ids.remove(1);
+        System.out.println(idToProduct);
+        System.out.println();
+
+    }
 }
